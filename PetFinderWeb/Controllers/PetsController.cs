36d063224c75fc0b1c182978ef49736863cbe31b
Andrew Matthews <matthews.andrew@gmail.com>
2020-10-14 14:50:47 +1100

    simple person display

---
+++ PetFinderWeb/Controllers/PetsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PetFinderWeb
+{
+    public class PetsController : Controller
+    {
+        private readonly ILogger<PetsController> _logger;
+
+        public PetsController(ILogger<PetsController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            var pf = new PetFinderCore.PetFinder(new PetFinderCore.PetFinderRepositoryClient("https://dorsavicodechallenge.azurewebsites.net/Melbourne"));
+            var people = (await pf.GetPeopleAsync()).Select(p => new Models.Person { Age = p.Age, Gender = p.Gender, Name = p.Name });
+            return View(people);
+        }
+    }
+}
---
+++ PetFinderWeb/Models/PersonViewModel.cs
@@ -0,0 +1,11 @@
+﻿using PetFinderCore;
+
+namespace PetFinderWeb.Models
+{
+    public class Person
+    {
+        public uint Age { get; set; }
+        public Gender Gender { get; set; }
+        public string Name { get; set; }
+    }
+}
